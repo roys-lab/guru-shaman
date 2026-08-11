d0ad78f23c756e86004459f26de78852e58f9f55
SH\roy.magarinos <roy.magarinos@spiritumentertainment.com>
2026-08-11 16:40:35 -0300

    Add bat

---
+++ run_server.bat
@@ -0,0 +1,7 @@
+@echo off
+title GuruShaman Server
+echo ==================================================
+echo   Starting GuruShaman Server...
+echo ==================================================
+node server.js
+pause
